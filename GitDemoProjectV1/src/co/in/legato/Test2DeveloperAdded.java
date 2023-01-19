f28024ceb7850b86a894c88041d359e174329ac1
Admin <pandupuli@gmail.com>
2023-01-19 13:20:41 +0530

    added additional checks

--- GitDemoProjectV1/src/co/in/legato/Test2DeveloperAdded.java
+++ GitDemoProjectV1/src/co/in/legato/Test2DeveloperAdded.java
@@ -5,6 +5,17 @@ public class Test2DeveloperAdded {
 	public static void main(String[] args) {
 
 		System.out.println("Developer pandupuli1 added this file...");
+		
+		String hungry = "yes";
+
+		if (hungry == "yes" || hungry == "Y" || hungry == "y") {
+			
+			System.out.println("eat something.....");
+			
+		} else {
+			
+			System.out.println("Do work.....");
+		}
 	}
 
 }
