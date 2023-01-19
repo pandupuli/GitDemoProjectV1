3c270e80fe709ef9e7c391a6d9e85d919ebae6c3
Admin <pandupuli@gmail.com>
2023-01-19 12:47:57 +0530

    commiting Test2DeveloperAdded.java to BranchX

---
+++ GitDemoProjectV1/src/co/in/legato/Test2DeveloperAdded.java
@@ -0,0 +1,10 @@
+package co.in.legato;
+
+public class Test2DeveloperAdded {
+
+	public static void main(String[] args) {
+
+		System.out.println("Developer pandupuli1 added this file...");
+	}
+
+}
