208eac21e858d14d9f7acd793a8d9d569d8d0505
Admin <pandupuli@gmail.com>
2023-01-19 10:46:53 +0530

    First commit on 19/01/23 at 10:46AM

---
+++ GitDemoProjectV1/src/co/in/legato/Test.java
@@ -0,0 +1,32 @@
+package co.in.legato;
+
+//Team lead created this project :
+// 1. Create new project
+//2.Create local (git) repository
+//3.Add files to staging
+//4.Commit changes to local repo
+//5.Push changes to Master Branch Remote Repo.
+
+//steps :
+//1. right click on project --> team --> share--> create local Repo.
+//2 .right click on project --> team -->add to index  : adds all files to staging area.
+//3. right click on project --> team --> commit : commits to local repo.
+
+public class Test {
+
+	public static void main(String[] args) {
+
+		String hungry = "yes";
+
+		if (hungry == "yes") {
+			
+			System.out.println("eat something.....");
+			
+		} else {
+			
+			System.out.println("Do work.....");
+		}
+
+	}
+
+}
